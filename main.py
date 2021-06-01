b56d8d76374116be2ec92f71d8831d36a1a323fc
GrimPerspective <>
2021-06-01 12:49:14 +0000

    Complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-player_input = input("What do you choose? Type 0 for Rock, 1 for paper or 2 for Scissors.")
+player_input = input("What do you choose? Type 0 for Rock, 1 for paper or 2 for Scissors. \n")
 
 rock = '''
     _______
